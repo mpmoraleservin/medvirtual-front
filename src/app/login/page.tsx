612d2a06a918832d1d7b4d0df4a772b1d4d36eaf
Lucas Nardon <lucaslnardon@gmail.com>
2025-06-10 20:49:31 -0300

    Update viewport size

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -25,6 +25,7 @@ export default function Login() {
     }
 
     useEffect(() => {
+        setViewport(getViewportWidth())
         window.addEventListener("resize", () => {
             setViewport(getViewportWidth())
         })
@@ -34,7 +35,7 @@ export default function Login() {
         <div className="relative w-full max-w-[1440px] flex flex-col  items-center p-4">
             <div className="flex items-center justify-between mb-16">
                 <img src="/logo.png" alt="company logo" className="" />
-                {viewport < 620 ? null : <a href="/signup" className="absolute top-5 right-5 h-fit font-bold text-white text-xl leading-[100%] px-6 py-4 rounded-xl bg-[var(--primary-color)] cursor-pointer">
+                {viewport < 640 ? null : <a href="/signup" className="absolute top-5 right-5 h-fit font-bold text-white text-xl leading-[100%] px-6 py-4 rounded-xl bg-[var(--primary-color)] cursor-pointer">
                     Sign up
                 </a>}
             </div>
