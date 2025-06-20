444dbd199e22bc3e2871384b4296ad8d4ab45fb0
Lucas Nardon <lucaslnardon@gmail.com>
2025-06-20 17:05:21 -0300

    Misc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="relative w-full max-w-[1440px] flex flex-col  items-center p-4">
+    <div className="relative w-full flex flex-col  items-center p-4">
 
     </div>
   );
